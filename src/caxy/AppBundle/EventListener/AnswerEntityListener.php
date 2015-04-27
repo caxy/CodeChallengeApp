40bbf17c69ecd8f88ff3a74b622a7f899219919c
Phil Stephenson <phil.stephenson@gmail.com>
2015-04-26 19:36:13 -0500

    Try a few new tricks

--- src/caxy/AppBundle/EventListener/AnswerEntityListener.php
+++ src/caxy/AppBundle/EventListener/AnswerEntityListener.php
@@ -8,16 +8,20 @@ use caxy\AppBundle\Entity\Answer;
 class AnswerEntityListener {
     public function postPersist(Answer $answer, LifecycleEventArgs $event) {
         $sandbox = new \PHPSandbox\PHPSandbox();
+        $sandbox->whitelist_keyword('function');
+        $sandbox->set_option('allow_functions', true);
         $cc_func_name = $answer->getCodeChallenge()->getName();
-        $sandbox->whitelist_func(array('implode', $cc_func_name));
+        $answerString = $answer->getAnswer();
+        $sandbox->define_func($cc_func_name, $answerString);
+        $sandbox->whitelist_func(array($cc_func_name, 'call_user_func_array'));
 
         $testCases = $answer->getCodeChallenge()->getTestCases();
 
         foreach ($testCases as $testCase) {
             if (count($testCase->getArgs()) > 1) {
                 $args = $testCase->getArgs();
-                $output = $sandbox->execute(function($cc_func_name, $args) {
-                    return $cc_func_name(implode(',', $args));
+                $output = $sandbox->execute(function() {
+                    return call_user_func_array($cc_func_name, $args);
                 });
 
                 // Compare $output to $testCase->output
