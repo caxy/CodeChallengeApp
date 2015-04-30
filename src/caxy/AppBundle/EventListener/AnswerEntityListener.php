e6cf1e5766f5aa62abfb7dbc67538867ba7bc0f4
Phil Stephenson <phil.stephenson@gmail.com>
2015-04-29 21:56:06 -0500

    More attempts

--- src/caxy/AppBundle/EventListener/AnswerEntityListener.php
+++ src/caxy/AppBundle/EventListener/AnswerEntityListener.php
@@ -8,15 +8,29 @@ use caxy\AppBundle\Entity\Answer;
 class AnswerEntityListener {
     public function postPersist(Answer $answer, LifecycleEventArgs $event) {
         $sandbox = new \PHPSandbox\PHPSandbox();
-        $sandbox->whitelist_keyword('function');
+        $sandbox->set_option('validate_functions', false);
+        $sandbox->set_option('allow_closures', true);
         $sandbox->set_option('allow_functions', true);
+        // $sandbox->whitelist_keyword('function');
+        // $sandbox->set_option('allow_functions', true);
         $cc_func_name = $answer->getCodeChallenge()->getName();
         $answerString = $answer->getAnswer();
-        $sandbox->define_func($cc_func_name, $answerString);
-        $sandbox->whitelist_func(array($cc_func_name, 'call_user_func_array'));
+        // $sandbox->define_func($cc_func_name, $answerString);
+        // $sandbox->whitelist_func(array($cc_func_name, 'sprintf'));
 
-        $testCases = $answer->getCodeChallenge()->getTestCases();
+        $sandbox->define_func($cc_func_name, function($a, $b) { return $b; });
 
+        // echo $answerString;
+
+        $output = $sandbox->execute(function($answerString) {
+            return call_user_func_array($answerString, array(1,4));
+        });
+
+        echo $output;
+
+        exit;
+        /*
+        // $testCases = $answer->getCodeChallenge()->getTestCases();
         foreach ($testCases as $testCase) {
             if (count($testCase->getArgs()) > 1) {
                 $args = $testCase->getArgs();
@@ -27,5 +41,6 @@ class AnswerEntityListener {
                 // Compare $output to $testCase->output
             }
         }
+        */
     }
 }
